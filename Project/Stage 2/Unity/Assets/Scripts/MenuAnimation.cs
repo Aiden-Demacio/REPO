5c3b8a2bb7c354a33342651ecf649a8a96f4e0f8
ArmoredNT <zachary.obrien@ontariotechu.net>
2024-12-10 21:37:36 -0500

    Menu Animation

--- Project/Stage 2/Unity/Assets/Scripts/MenuAnimation.cs
+++ Project/Stage 2/Unity/Assets/Scripts/MenuAnimation.cs
@@ -1,16 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class MenuAnimation : MonoBehaviour
 {
     [SerializeField] private GameObject brokenPrefab;
-
+    [SerializeField] private PlayableDirector director;
+    
     private bool hasClicked;
     // Start is called before the first frame update
+    private void Awake()
+    {
+        //director.Play();
+    }
+
     void Start()
     {
-        
+        //director.Stop();
     }
 
     // Update is called once per frame
@@ -18,11 +27,14 @@ public class MenuAnimation : MonoBehaviour
     {
         if (Input.GetButtonDown("Fire1") && !hasClicked)
         {
+            director.Play();
             hasClicked = true;
             gameObject.GetComponent<MeshRenderer>().enabled = false;
 
-            Instantiate(brokenPrefab, transform);
+            Transform t = Instantiate(brokenPrefab, transform.position, quaternion.identity).transform;
+            t.transform.rotation = transform.rotation;
             
+            Destroy(t.gameObject, 3);
             Destroy(this, 3);
         }
     }
